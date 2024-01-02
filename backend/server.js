4e8fc7b29be283bf5f879d4a80070af9d8153ff3
Minh Khoi Dev <mkbentre@gmail.com>
2024-01-02 23:38:19 +0700

    'ssl'

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
-var express = require('express')
+var fs = require('fs');
+// var http = require('http');
+var https = require('https');
+var express = require('express');
 var app = express()
+var privateKey = fs.readFileSync( 'privatekey.pem' );
+var certificate = fs.readFileSync( 'certificate.pem' );
 require('dotenv').config()
 var AuthMiddleWare = require('./app/common/AuthMiddleWare')
 
@@ -12,6 +17,11 @@ app.use(AuthMiddleWare.isAuth);
 require('./app/routers/wallet.router')(app);
 require('./app/routers/cataloge.router')(app);
 require('./app/routers/transaction.router')(app);
-app.listen(process.env.PORT, function() {
-    console.log("Server started on PORT: " + process.env.PORT);
-});
+// app.listen(process.env.PORT, function() {
+//     console.log("Server started on PORT: " + process.env.PORT);
+// });
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(process.env.PORT);
